540d18d4cd060080425528bfd2552c67fbacb66b
Andrey Semka <k9nz00@yandex.ru>
2019-12-09 23:41:27 +0300

    переименовал и перенес папку для роутов

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once 'bootstrap.php';
-require_once 'web/route.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/bootstrap.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/src/route/route.php';
 
 use App\Application;
 use App\Router;
